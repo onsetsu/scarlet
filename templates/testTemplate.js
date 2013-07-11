de64289768b659c9451e01c66fa43f8cd29fa10f
Stefan Lehmann <stefan.lehmann@student.hpi.uni-potsdam.de>
2013-07-11 16:40:02 +0200

    rewrote testTemplate to use sinon.testCase

--- templates/testTemplate.js
+++ templates/testTemplate.js
@@ -1,16 +1,27 @@
-TestCase("testTemplate", 
-{
+TestCase("testTemplate", sinon.testCase({
 	setUp: function() 
-	{		
+	{	
 	},
 	
 	tearDown: function()
 	{
 	},
 		
-	// Onsetsu.Template is defined
-	"testTemplateDefined": function()
-	{	
-		assertNotUndefined(Onsetsu.Template);
-	}
-});
+	// TODO: write comment.
+	"testSomeName": sinon.test(function() {
+		var callbackContext = {
+				callbackFunction: function() {}
+		};
+		var callbackArg1 = "foo";
+		var callbackArg2 = "bar";
+	    this.mock(callbackContext)
+	    	.expects("callbackFunction")
+	    	.on(callbackContext)
+	    	.once()
+	    	.withExactArgs(callbackArg1, callbackArg2);
+
+	    var callback = new Scarlet.Callback(callbackContext, callbackContext.callbackFunction);
+	    callback.execute(callbackArg1, callbackArg2);
+	})
+	
+}));
