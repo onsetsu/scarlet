bbd2bf657f3255ad5d6fa3a55ff983222cf7db78
Stefan Lehmann <stefan.lehmann@student.hpi.uni-potsdam.de>
2013-07-11 15:32:22 +0200

    removed unnecessary examples

--- examples/_t3_facebook/The Facebook Offering  How It Compares - NYTimes.com-Dateien/meter.js
+++
@@ -1,1 +0,0 @@
-c1359070991478({"hitPaywall":false,"counted":true,"loggedIn":false,"hash":"2DC7892B3C8BB344E3B5909FBBE6400B"});
--- examples/_threed/constants.js
+++
@@ -1,7 +0,0 @@
-var appConstants  = {
-
-    TRANSLATE_0 : -360,
-    TRANSLATE_1 : 10040,
-    SCALE : 60000
-
-}
