36b7d34d8f4264493c780592f19b5c1fdc2086f6
Stefan Lehmann <stefan.lehmann@student.hpi.uni-potsdam.de>
2013-07-18 18:37:04 +0200

    add convinient methods for testing

--- tests/config.js
+++ tests/config.js
@@ -4,3 +4,11 @@
  */
 //sinon.assert.fail = function(){};
 //var assertUndefined = function assertUndefined()
+/*
+assertContains = function(list, element) {
+	return assertTrue("Element " + element + " is supposed to be in list " + list + ", but was not.", _.contains(list, element));
+};
+assertNotContains = function(list, element) {
+	return assertFalse("Element " + element + " is not supposed to be in list " + list + ", but it was.", !(_.contains(list, element)));
+};
+*/
