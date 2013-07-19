f374212ac429b22427ff43f4eaed541e15d84f3b
Stefan Lehmann <stefan.lehmann@student.hpi.uni-potsdam.de>
2013-07-19 07:50:45 +0200

    extended tests for signal ans slot

--- tests/core/testSignalslot.js
+++ tests/core/testSignalslot.js
@@ -19,15 +19,18 @@ TestCase("testSignalslot", sinon.testCase({
 		var spySlot1 = this.spy(firstSlot, "execute");
 		var spySlot2 = this.spy(secondSlot, "execute");
 		
+		var emitArgument = "blub";
 		firstSignal
 			.connect(firstSlot);
 		secondSignal
 			.connect(firstSlot)
 			.connect(secondSlot)
-			.emit();
+			.emit(emitArgument);
 		sinon.assert.calledOnce(spySlot1);
 		sinon.assert.calledOnce(spySlot2);
 		sinon.assert.callOrder(spySlot1, spySlot2);
+		sinon.assert.calledWith(spySlot1, emitArgument);
+		sinon.assert.calledWith(spySlot2, emitArgument);
 		//assert(spySlot1.calledBefore(spySlot2), "slots were not called in the order they were connected.");
 	}
 	
