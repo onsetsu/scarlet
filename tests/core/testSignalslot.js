a745461e347b3aedf57092af1f7470d1b575df8d
Stefan Lehmann <stefan.lehmann@student.hpi.uni-potsdam.de>
2013-07-19 07:55:18 +0200

    insure that given method is called in test of signal and slot

--- tests/core/testSignalslot.js
+++ tests/core/testSignalslot.js
@@ -9,12 +9,13 @@ TestCase("testSignalslot", sinon.testCase({
 
 	// Scarlet.Signal .
 	"testSignalAPI": function()
-	{	
+	{
 		var firstSignal = new Scarlet.Signal();
 		var secondSignal = new Scarlet.Signal();
 		
-		var firstSlot = new Scarlet.Slot({}, function() {});
-		var secondSlot = new Scarlet.Slot({}, function() {});
+		var foo = 1;
+		var firstSlot = new Scarlet.Slot({}, function() { foo *= 2; });
+		var secondSlot = new Scarlet.Slot({}, function() { foo *= 3; });
 
 		var spySlot1 = this.spy(firstSlot, "execute");
 		var spySlot2 = this.spy(secondSlot, "execute");
@@ -31,6 +32,7 @@ TestCase("testSignalslot", sinon.testCase({
 		sinon.assert.callOrder(spySlot1, spySlot2);
 		sinon.assert.calledWith(spySlot1, emitArgument);
 		sinon.assert.calledWith(spySlot2, emitArgument);
+		assertEquals(2 * 3, foo);
 		//assert(spySlot1.calledBefore(spySlot2), "slots were not called in the order they were connected.");
 	}
 	
